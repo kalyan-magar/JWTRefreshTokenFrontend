b244881a29f99dda4d90d94828186cb4272296e8
Kalyan <kalyan@ATPLTECH.local>
2025-05-02 17:00:18 +0530

    register form

--- src/components/Users.js
+++ src/components/Users.js
@@ -17,7 +17,7 @@ const Users = () => {
 
         const getUsers = async () => {
             try {
-                const response = await axiosPrivate.get('http://localhost:3500/users/', {
+                const response = await axiosPrivate.get('/users', {
                     signal: controller.signal
                 });
                 console.log(response.data);
